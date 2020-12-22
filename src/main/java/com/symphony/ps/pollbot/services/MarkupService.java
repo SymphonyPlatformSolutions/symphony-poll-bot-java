06168afea08b14918b1b45a97c9210d7de4169a9
symphony-hong <65538951+symphony-hong@users.noreply.github.com>
2020-12-22 00:44:39 -0800

    PLAT-9983: Migrate Poll Bot to BDK2.0 (#3)
    * Migrate to BDK2.0
    
    * Update Youri's comments
    
    Use String#join instead of StringUtils#join to get rid of commons-lang lib.
    
    Migrate bot configuration from config.json to application.properties.
    
    Well-handle the stream type of the incoming message to be sure that the bot will not down if there's any new stream type in future.
    
    Clean up the pom file.

--- src/main/java/com/symphony/ps/pollbot/services/MarkupService.java
+++ src/main/java/com/symphony/ps/pollbot/services/MarkupService.java
@@ -1,59 +1,38 @@
 package com.symphony.ps.pollbot.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.symphony.ps.pollbot.model.Poll;
 import com.symphony.ps.pollbot.model.PollBlastData;
 import com.symphony.ps.pollbot.model.PollCreateData;
 import com.symphony.ps.pollbot.model.PollData;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
-import java.util.List;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Slf4j
 class MarkupService {
-    private static ObjectMapper mapper = new ObjectMapper();
-    static String pollCreateTemplate = loadTemplate("/poll-create-form.ftl");
-    static String pollBlastTemplate = loadTemplate("/poll-blast-form.ftl");
-    static String pollResultsTemplate = loadTemplate("/poll-results.ftl");
-    static String pollHistoryTemplate = loadTemplate("/poll-history.ftl");
 
-    private static String loadTemplate(String fileName) {
-        InputStream stream = MarkupService.class.getResourceAsStream(fileName);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
-            return br.lines().collect(Collectors.joining(System.lineSeparator()));
-        }
-        catch (IOException e) {
-            log.error("Unable to load template for {}", fileName);
-            return null;
-        }
+    static String pollCreateTemplate = "/poll-create-form.ftl";
+    static String pollBlastTemplate = "/poll-blast-form.ftl";
+    static String pollResultsTemplate = "/poll-results.ftl";
+    static String pollHistoryTemplate = "/poll-history.ftl";
+
+    static Map<String, PollData> getPollCreateData(boolean showPersonSelector, String targetStreamId, int count, List<Integer> timeLimits) {
+        return Collections.singletonMap("data", new PollCreateData(
+                showPersonSelector,
+                targetStreamId,
+                count,
+                timeLimits));
     }
 
-    static String getPollCreateData(boolean showPersonSelector, String targetStreamId, int count, List<Integer> timeLimits) {
-        return convertToJsonString(new PollCreateData(showPersonSelector, targetStreamId, count, timeLimits));
-    }
-
-    static String getPollBlastData(Poll poll) {
-        return convertToJsonString(new PollBlastData(
-            poll.getId() + "",
-            poll.getTimeLimit(),
-            poll.getQuestionText(),
-            poll.getAnswers(),
-            poll.getCreatorDisplayName()
+    static Map<String, PollData> getPollBlastData(Poll poll) {
+        return Collections.singletonMap("data", new PollBlastData(
+                poll.getId() + "",
+                poll.getTimeLimit(),
+                poll.getQuestionText(),
+                poll.getAnswers(),
+                poll.getCreatorDisplayName()
         ));
-    }
-
-    static String convertToJsonString(PollData data) {
-        try {
-            return mapper.writeValueAsString(data);
-        } catch (JsonProcessingException e) {
-            log.error("Unable to wrap data object: {}", e.getMessage());
-            return null;
-        }
     }
 }
